fd26b74509723d610c51434693365586efd09cd7
Rukai <rubickent@gmail.com>
2017-02-13 07:07:12 +1100

    Fixed colour on opengl backend

--- pf_engine/src/shaders/generic-fragment.glsl
+++ pf_engine/src/shaders/generic-fragment.glsl
@@ -4,7 +4,7 @@
 #extension GL_ARB_shading_language_450pack : enable
 
 layout(location = 0) in float v_edge;
-layout(location = 1) in float render_id;
+layout(location = 1) in float v_render_id;
 layout(location = 0) out vec4 f_color;
 
 layout(set = 0, binding = 0) uniform Data {
@@ -17,10 +17,10 @@ layout(set = 0, binding = 0) uniform Data {
 } uniforms;
 
 void main() {
-    if (render_id == 0.0) {
+    if (v_render_id == 0.0) {
         f_color = uniforms.color;
     }
-    else if (render_id == 1.0) {
+    else if (v_render_id == 1.0) {
         if (v_edge > 0.8) {
             f_color = uniforms.edge_color;
         }
@@ -28,22 +28,22 @@ void main() {
             f_color = uniforms.color;
         }
     }
-    else if (render_id == 2.0) {
+    else if (v_render_id == 2.0) {
         f_color = vec4(1.0, 0.0, 0.0, 1.0);
     }
-    else if (render_id == 3.0) {
+    else if (v_render_id == 3.0) {
         f_color = vec4(0.76, 0.106, 0.843, 1.0);
     }
-    else if (render_id == 4.0) {
+    else if (v_render_id == 4.0) {
         f_color = vec4(0.52, 0.608, 0.756, 1.0);
     }
-    else if (render_id == 5.0) {
+    else if (v_render_id == 5.0) {
         f_color = vec4(0.0, 0.64, 0.0, 1.0);
     }
-    else if (render_id == 6.0) {
+    else if (v_render_id == 6.0) {
         f_color = vec4(0.8, 0.8, 0.8, 1.0);
     }
-    else if (render_id == 7.0) {
+    else if (v_render_id == 7.0) {
         f_color = vec4(0.0, 0.0, 1.0, 1.0);
     }
 }
--- pf_engine/src/shaders/opengl-generic-fragment.glsl
+++ pf_engine/src/shaders/opengl-generic-fragment.glsl
@@ -4,7 +4,7 @@
 // TODO: block uniforms are really annoying to handle in glium
 
 in float v_edge;
-in float render_id;
+in float v_render_id;
 out vec4 f_color;
 
 uniform vec2  position_offset;
@@ -15,10 +15,10 @@ uniform vec4  edge_color;
 uniform vec4  color;
 
 void main() {
-    if (render_id == 0.0) {
+    if (v_render_id == 0.0) {
         f_color = color;
     }
-    else if (render_id == 1.0) {
+    else if (v_render_id == 1.0) {
         if (v_edge > 0.8) {
             f_color = edge_color;
         }
@@ -26,22 +26,22 @@ void main() {
             f_color = color;
         }
     }
-    else if (render_id == 2.0) {
+    else if (v_render_id == 2.0) {
         f_color = vec4(1.0, 0.0, 0.0, 1.0);
     }
-    else if (render_id == 3.0) {
+    else if (v_render_id == 3.0) {
         f_color = vec4(0.76, 0.106, 0.843, 1.0);
     }
-    else if (render_id == 4.0) {
+    else if (v_render_id == 4.0) {
         f_color = vec4(0.52, 0.608, 0.756, 1.0);
     }
-    else if (render_id == 5.0) {
+    else if (v_render_id == 5.0) {
         f_color = vec4(0.0, 0.64, 0.0, 1.0);
     }
-    else if (render_id == 6.0) {
+    else if (v_render_id == 6.0) {
         f_color = vec4(0.8, 0.8, 0.8, 1.0);
     }
-    else if (render_id == 7.0) {
+    else if (v_render_id == 7.0) {
         f_color = vec4(0.0, 0.0, 1.0, 1.0);
     }
 }
